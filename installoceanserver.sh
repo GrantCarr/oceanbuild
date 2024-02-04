d95a209db5ddc79d3ee7576c50345d4ff67cfe83
GrantCarr <36683467+GrantCarr@users.noreply.github.com>
2024-02-04 16:21:20 +1000

    Update installoceanserver.sh

--- installoceanserver.sh
+++ installoceanserver.sh
@@ -7,14 +7,9 @@ echo "-----------------------------------------------------------Setting SNMP"
 apt install -y snmp snmpd snmp-mibs-downloader libnet-snmp-perl snmptrapd
 sed -i 's/mibs :/# mibs :/g' /etc/snmp/snmp.conf
 sed -i '/^[^#]/ s/\(^.*agentaddress.*$\)/#\ \1/' /etc/snmp/snmpd.conf
-sed -i '/^[^#]/ s/\(^.*rocommunity.*$\)/#\ \1/' /etc/snmp/snmpd.conf
-sed -i '/^[^#]/ s/\(^.*rocommunity6.*$\)/#\ \1/' /etc/snmp/snmpd.conf
-
 echo "agentaddress udp:161,udp6:[::1]:161" > /etc/snmp/snmpd.conf.d/allin.conf
 echo "view   systemonly  included   .1.3.6.1.2.1" >> /etc/snmp/snmpd.conf.d/allin.conf
 echo "view   systemonly  included   .1.3.6.1.4.1" >> /etc/snmp/snmpd.conf.d/allin.conf
-echo "rocommunity  vinraystreet" >> /etc/snmp/snmpd.conf.d/allin.conf
-echo "rocommunity6 vinraystreet" >> /etc/snmp/snmpd.conf.d/allin.conf
 systemctl restart snmpd
 ufw allow 161/udp
 ufw allow 162/udp
