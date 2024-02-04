85d77c5b8264c330c4dbc2c79e7fd6f9dc5f1d3a
GrantCarr <36683467+GrantCarr@users.noreply.github.com>
2024-02-04 20:52:10 +1000

    more in install

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-echo "this is working with extra spaces"
+echo "-----------------------------------------------------------Setting SNMP"
 apt install -y snmp snmpd snmp-mibs-downloader
+sed -i 's/mibs :/# mibs :/g' /etc/snmp/snmp.conf
+systemctl stop snmpd
+cp -f ./oceansnmpd.conf /etc/snmp/snmpd.conf
+systemctl restart snmpd
+ufw allow 161/udp
+ufw allow 162/udp
+
+
+echo "-----------------------------------------------------------Adding user grant"
+adduser --disabled-password --gecos "" grant
+passwd grant
+adduser grant sudo
+
+
 
 
