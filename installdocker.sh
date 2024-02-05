a54bca1bf62393d782500606582f7acacf663604
GrantCarr <36683467+GrantCarr@users.noreply.github.com>
2024-02-05 20:24:23 +1000

    docker files

---
+++ installdocker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl
+
+sudo adduser grant docker
+sudo adduser ansible docker
+
+mkdir docker && cd docker
+mkdir dockge
+# cp ./../dockge.yaml ./dockge/dockge.yaml
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+echo "---------------------------------------reboot before docker compose up -d"
+echo ""
+echo ""
+
