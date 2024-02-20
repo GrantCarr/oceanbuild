b2710af73b5ff38f10e997626c6c906b6bc2f6c1
GrantCarr <grant@stonechilli.com.au>
2024-02-20 10:13:12 +1000

    install docker and dockge

---
+++ installdockerdockge.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "----------------------------------------------install docker using help script"
+mkdir /home/grant/docker 
+
+curl -fsSL https://get.docker.com -o ./docker/get-docker.sh
+sh ./docker/get-docker.sh
+
+sudo adduser grant docker
+sudo adduser ansible docker
+
+echo "----------------------------------------------install dockge using docker compose up"
+mkdir /home/grant/docker/dockge
+#wget compose file
+wget https://raw.githubusercontent.com/GrantCarr/oceanbuild/main/dockge.yaml
+mv ./dockge.yaml /home/grant/docker/dockge/compose.yaml
+# docker compose up -d - needs sudo as adduser won't have taken
+sudo docker compose -f /home/grant/docker/dockge/compose.yaml up -d
+#
+echo ""
+echo ""
+echo hostname
+echo hostname -I
+echo ""
+echo "----------------------------------------------dockge on port: 5001"
+echo ""
+echo ""
